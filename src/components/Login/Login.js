3968943527e6fc4603f331f99c0f96b983bcb2d0
Fernanda Dehays <fernanda.dehays@mtc.berlin>
2022-09-20 13:15:07 +0200

    centralize the login & logut logic in auth-context <- cleaner compnts

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
 	if (action.type === 'USER_INPUT') {
@@ -49,6 +50,8 @@ const Login = (props) => {
 
 	const { isValid: passwordIsValid } = passwordState;
 
+	const authCtx = useContext(AuthContext)
+
 	useEffect(() => {
 		const identifier = setTimeout(() => {
 			setFormIsValid(emailIsValid && passwordIsValid);
@@ -89,7 +92,7 @@ const Login = (props) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		props.onLogin(emailState.value, passwordState.value);
+		authCtx.onLogin(emailState.value, passwordState.value);
 	};
 
 	return (
